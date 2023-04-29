c95fda8435332f39f215e46665e2d3f6c45832f9
Volodymyr Rud <vova.rud.00@gmail.com>
2023-04-29 18:57:07 +0300

    Added basic .gif decoding, changed architecture.

--- VR.GifUpscaler.View/App.xaml.cs
+++ VR.GifUpscaler.View/App.xaml.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using VR.GifUpscaler.ViewModel.ViewModels;
 
 namespace VR.GifUpscaler.View
 {
@@ -13,5 +12,40 @@ namespace VR.GifUpscaler.View
     /// </summary>
     public partial class App : Application
     {
+        private readonly IHost _host;
+
+        public App()
+        {
+            _host = Host.CreateDefaultBuilder().ConfigureServices(ConfigureServices).Build();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<MainViewModel>();
+            services.AddSingleton<MainWindow>(services => new MainWindow
+            {
+                DataContext = services.GetService<MainViewModel>()
+            });
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await _host.StartAsync();
+
+            var mainWindow = _host.Services.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            using (_host)
+            {
+                await _host.StopAsync();
+            }
+
+            base.OnExit(e);
+        }
     }
 }
---
+++ VR.GifUpscaler.ViewModel/ViewModels/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace VR.GifUpscaler.ViewModel.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        public ImageSource _gifFrame;
+
+        [RelayCommand]
+        public async void RunGif()
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var gifPath = currentDirectory + "\\Pigs.gif";
+
+            Stream imageStreamSource = new FileStream(gifPath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            GifBitmapDecoder decoder = new GifBitmapDecoder(imageStreamSource, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+
+            //GifFrame = decoder.Frames[0];
+
+            for (int i = 0; i < decoder.Frames.Count(); i++)
+            {
+                await Application.Current.Dispatcher.InvokeAsync(() =>
+                {
+                    GifFrame = decoder.Frames[i];
+                    Thread.Sleep(30);
+                }, DispatcherPriority.Background);
+            }
+        }
+    }
+}
